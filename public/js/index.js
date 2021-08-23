5d31d6d531dc366c1db070df0ac6f2eab747cba8
quistty <davequist22@gmail.com>
2021-08-23 09:17:02 -0400

    removing the api key with .gitignore and doing some js sowrk

--- public/js/index.js
+++ public/js/index.js
@@ -4,3 +4,12 @@ let apiFile = require('./credentials/apiKey.json');
 let apiKey = apiFile["apiKey"]
 
 const playerName = "Porsha_boy"
+const playerUUID = "e"
+
+
+fetch(`https://api.hypixel.net/player?key=${apiKey}&name=${playerName}`)  // fetching from the api
+    .then(response => response.json()) // turning the info from the api from bytes to a json
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => console.log("Netorking error", error)) // this is just to catch any errors
