6fc30f1851218c74b3754f8612937ded80f03c96
quistty <davequist22@gmail.com>
2021-08-26 11:07:54 -0400

    recognizes the api key but github will not

--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,21 @@
 const fetch = require('node-fetch');
-let apiFile = require('../credentials/apiKey.json');
+let apiFile = require('../apiKey.json');
 
 let apiKey = apiFile["apiKey"]
 
-const playerName = "Porsha_boy"
+//const playerName = "Porsha_boy"
+const ign = ""
 const playerUUID = "e"
 
 
-fetch(`https://api.hypixel.net/player?key=${apiKey}&name=${playerName}`)  // fetching from the api
+
+fetch(`https://api.ashcon.app/mojang/v2/user/${ign}`)
+
+fetch(`https://api.hypixel.net/player?key=${apiKey}&name=${ign}`)  // fetching from the api
     .then(response => response.json()) // turning the info from the api from bytes to a json
     .then(data => {
         console.log(data)
     })
     .catch(error => console.log("Netorking error", error)) // this is just to catch any errors
 
-    //function please work on this
+    // function please work on this
