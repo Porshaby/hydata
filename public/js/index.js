b600845a0aeb9125aae6659b308f8b18506c31e6
quistty <davequist22@gmail.com>
2021-08-27 11:29:41 -0400

    seperating the player search and player stats

--- public/js/index.js
+++ public/js/index.js
@@ -10,7 +10,8 @@ const playerUUID = "e"
 
 
 fetch(`https://api.ashcon.app/mojang/v2/user/${ign}`)
-
+function apicontact() {
+    
 fetch(`https://api.hypixel.net/player?key=${apiKey}&name=${ign}`)  // fetching from the api
     .then(response => response.json()) // turning the info from the api from bytes to a json
     .then(data => {
@@ -19,3 +20,5 @@ fetch(`https://api.hypixel.net/player?key=${apiKey}&name=${ign}`)  // fetching f
     .catch(error => console.log("Netorking error", error)) // this is just to catch any errors
 
     // function please work on this
+
+}
