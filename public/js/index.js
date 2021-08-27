bef9e1c0cd302ff32bf55618791fd1176ecdbe5c
quistty <davequist22@gmail.com>
2021-08-27 12:52:03 -0400

    content now

--- public/js/index.js
+++ public/js/index.js
@@ -4,21 +4,25 @@ let apiFile = require('../apiKey.json');
 let apiKey = apiFile["apiKey"]
 
 //const playerName = "Porsha_boy"
-const ign = ""
+const ign = "midnight_flash"
 const playerUUID = "e"
 
 
 
 fetch(`https://api.ashcon.app/mojang/v2/user/${ign}`)
-function apicontact() {
-    
-fetch(`https://api.hypixel.net/player?key=${apiKey}&name=${ign}`)  // fetching from the api
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch(error => console.log("networking error", error))
+
+
+//fetch(`https://api.hypixel.net/player?key=${apiKey}&name=${ign}`)  // fetching from the api
     .then(response => response.json()) // turning the info from the api from bytes to a json
     .then(data => {
         console.log(data)
     })
-    .catch(error => console.log("Netorking error", error)) // this is just to catch any errors
+    .catch(error => console.log("An error has occured, this is the messsage:", error)) // this is just to catch any errors
 
     // function please work on this
 
-}
